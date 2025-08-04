0f993c81627f2ba4a8357e011f6f8f980a877fd8
kamlesh-cms <87517041+kamlesh-cms@users.noreply.github.com>
2025-08-04 12:46:07 +0530

    update api and app file

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -15,6 +15,7 @@ app.use(express.json({ limit: '16kb' }));
 
 // Parse incoming URL-encoded requests with a size limit
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+app.use('/api', Router);
 
 // --- Health Check Route ---
 app.get('/', (req, res) => {
