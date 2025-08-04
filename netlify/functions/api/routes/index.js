0925754ac75abaae7a21084c28838d9af712bd2a
kamlesh-cms <87517041+kamlesh-cms@users.noreply.github.com>
2025-08-04 13:10:11 +0530

    update test route

--- netlify/functions/api/routes/index.js
+++ netlify/functions/api/routes/index.js
@@ -1,13 +1,15 @@
-// src/api/routes/index.js
-
 import { Router } from 'express';
-import authRouter from './auth.routes.js';
-import userRouter from './user.routes.js';
+import {app} from "../../app.js";
+//import authRouter from './auth.routes.js';
+//import userRouter from './user.routes.js';
 
 const router = Router();
+router.get('/me', (req, res) => {
+    res.status(200).json({ status: 'ok', message: 'API is healthy and running me!' });
+});
 
 // Mount the individual routers
-router.use('/auth', authRouter);
-router.use('/users', userRouter);
+//router.use('/auth', authRouter);
+//router.use('/users', userRouter);
 
 export default router;
