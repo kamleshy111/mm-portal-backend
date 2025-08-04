ecd810f7ffda5b5bdc740e7ae2476e57db8252cd
kamlesh-cms <87517041+kamlesh-cms@users.noreply.github.com>
2025-08-04 13:04:58 +0530

    update api and app file

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -2,8 +2,10 @@ import express, { Router } from "express";
 import cors from 'cors';
 import { env } from './config/environment.js';
 import { errorHandler } from './api/middlewares/errorHandler.middleware.js';
+import apiRouter from './api/routes/index.js';
+
 const app = express();
-const router = Router();
+//const router = Router();
 // Enable Cross-Origin Resource Sharing (CORS)
 app.use(cors({
     origin: env.CORS_ORIGIN,
@@ -15,44 +17,18 @@ app.use(express.json({ limit: '16kb' }));
 
 // Parse incoming URL-encoded requests with a size limit
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+// Serve static files from the 'public' directory
+app.use('/public', express.static('public'));
+
 
 // --- Health Check Route ---
-router.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.status(200).json({ status: 'ok', message: 'API is healthy and running!' });
 });
 
-router.get('/test-api', (req, res) => {
-    res.json({ status: 'ok', message: 'API is healthy and running!' });
-});
 
 app.use(errorHandler);
 
-app.use("/api/", router);
+//app.use("/api/", router);
+app.use('/api/', apiRouter);
 export { app };
-
-/*
-const api = express();
-api.use(express.json()); // For parsing JSON request bodies
-
-const router = Router();
-
-// In-memory array to store records
-let records = [
-    { id: 1, name: "Record 1" },
-    { id: 2, name: "Record 2" }
-];
-
-// Read all records
-router.get("/", (req, res) => {
-    res.json(records);
-});
-
-// Read a single record by ID
-router.get("/:id", (req, res) => {
-    const record = records.find(r => r.id === parseInt(req.params.id));
-    if (!record) return res.status(404).json({ message: "Record not found" });
-
-    res.json(record);
-});
-
- */
