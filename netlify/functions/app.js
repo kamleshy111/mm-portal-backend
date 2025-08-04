1eaf6160e1f002b8f56723cbe915d19bdb0f9e35
kamlesh-cms <87517041+kamlesh-cms@users.noreply.github.com>
2025-08-04 14:33:24 +0530

    update test route

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -1,36 +1,21 @@
-import express, { Router } from "express";
+import express from "express";
 import cors from 'cors';
 import { env } from './config/environment.js';
 import { errorHandler } from './api/middlewares/errorHandler.middleware.js';
 import apiRouter from './api/routes/index.js';
 
 const app = express();
-// Enable Cross-Origin Resource Sharing (CORS)
-app.use(cors({
-    origin: env.CORS_ORIGIN,
-    credentials: true,
-}));
-
-// Parse incoming JSON requests with a size limit
+app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
 app.use(express.json({ limit: '16kb' }));
-
-// Parse incoming URL-encoded requests with a size limit
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
-// Serve static files from the 'public' directory
 app.use('/public', express.static('public'));
+
+app.get('/', (req, res) => res.status(200).json({ status: 'ok', message: 'API is healthy and running!' }));
+app.get('/test', (req, res) => res.status(200).json({ status: 'ok', message: 'API is healthy and running!' }));
 
 app.use('/api/', apiRouter);
 
-// --- Health Check Route ---
-app.get('/', (req, res) => {
-    res.status(200).json({ status: 'ok', message: 'API is healthy and running!' });
-});
-app.get('/test', (req, res) => {
-    res.status(200).json({ status: 'ok', message: 'API is healthy and running!' });
-});
-
-
-//app.use(errorHandler);
+app.use(errorHandler);
 
 //app.use("/api/", router);
 
