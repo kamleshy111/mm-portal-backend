faba48ab9564bd716875fe5a96f563a3b4f2eab4
kamlesh-cms <87517041+kamlesh-cms@users.noreply.github.com>
2025-08-04 18:57:35 +0530

    update stru

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,11 +1,4 @@
-import express, { Router } from "express";
-import serverless from "serverless-http";
+import serverless from 'serverless-http';
+import { app } from './app.js';
 
-const api = express();
-
-const router = Router();
-router.get("/hello", (req, res) => res.send("Hello World!"));
-
-api.use("/api/", router);
-
-export const handler = serverless(api);
+export const handler = serverless(app);
--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -1,12 +1,10 @@
-import express from 'express';
+import express, { Router } from "express";
 import cors from 'cors';
 import { env } from './config/environment.js';
 import { errorHandler } from './api/middlewares/errorHandler.middleware.js';
 import apiRouter from './api/routes/index.js';
 
 const app = express();
-
-// Enable Cross-Origin Resource Sharing (CORS)
 app.use(cors({
     origin: env.CORS_ORIGIN,
     credentials: true,
@@ -21,12 +19,6 @@ app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 // Serve static files from the 'public' directory
 app.use('/public', express.static('public'));
 
-// --- API Routes ---
-// All API routes will be prefixed with /api
-app.use('/api', apiRouter);
-
-
-// --- Health Check Route ---
 app.get('/', (req, res) => {
     res.status(200).json({ status: 'ok', message: 'API is healthy and running!' });
 });
@@ -35,9 +27,12 @@ app.get('/test-api', (req, res) => {
     res.status(200).json({ status: 'ok', message: 'API is healthy and running!' });
 });
 
+app.use('/api', apiRouter);
 
-// --- Global Error Handler ---
-// This middleware catches all errors passed to next()
+const router = Router();
+router.get("/hello", (req, res) => res.send("Hello World!"));
+
+app.use("/api11/", router);
+
 app.use(errorHandler);
-
 export { app };
