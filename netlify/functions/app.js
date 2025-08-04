3444dcc7327d219a6d8bc07569437691e95781b0
kamlesh-cms <87517041+kamlesh-cms@users.noreply.github.com>
2025-08-04 15:30:53 +0530

    update routes

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import { errorHandler } from "./api/middlewares/errorHandler.middleware.js";
 import apiRouter from "./api/routes/index.js";
-import router from "./api/routes/index.js"; // ✅ correct relative path
 
 const app = express();
 
@@ -10,7 +9,7 @@ app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
-app.use("/api", apiRouter.default) // ✅ this mounts /api from your router
-app.use(errorHandler);      // ✅ must be after routes
+app.use("/api", apiRouter);
+app.use(errorHandler);
 
 export { app };
